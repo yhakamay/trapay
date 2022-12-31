5a736645aecfa4e676318d32f4fe409a2d49c189
Yusuke <yhakamaya@adobe.com>
2023-01-01 01:35:15 +0900

    add auth gate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
 import Head from "next/head";
-import { Box, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  Text,
+  VStack,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { events } from "../events";
 import EventCard from "../components/molecules/event_card";
+import { SignInButton } from "../components/molecules/sign_in_button";
+import { auth } from "../firebaseConfig";
 
 export default function Home() {
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return (
+      <>
+        <Container py={{ base: "12", md: "24" }}>
+          <VStack spacing="8">
+            <Heading>TraPay</Heading>
+            <Text>Split the bill with your friends easily!</Text>
+            <SignInButton />
+            <Image src="/hello.svg" alt="hello" boxSize="300px" />
+          </VStack>
+        </Container>
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
