6683f29c980ecac6d3e8e723d441175040629c1d
Yusuke <yhakamaya@adobe.com>
2022-12-31 22:01:56 +0900

    add home which lists all the events

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import { Card, CardBody, Center } from "@chakra-ui/react";
+import Head from "next/head";
+import { Box, Wrap } from "@chakra-ui/react";
+import { EventCard } from "../components/molecules/event_card";
+import { events } from "../events";
 
 export default function Home() {
   return (
     <>
-      <Center>
-        <Card>
-          <CardBody>Home</CardBody>
-        </Card>
-      </Center>
+      <Head>
+        <title>TraPay</title>
+        <meta
+          name="description"
+          content="Split the bill with your friends easily!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box px={{ base: "4", md: "8" }}>
+        <Wrap>
+          {events.map((event) => (
+            <EventCard
+              key={event.id}
+              id={event.id.toString()}
+              title={event.title}
+              createdAt={event.createdAt}
+              description={event.description}
+            />
+          ))}
+        </Wrap>
+      </Box>
     </>
   );
 }
