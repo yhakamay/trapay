8fff9b668b8be82c61efe10f4aa6108c1144f26a
Yusuke <yhakamaya@adobe.com>
2022-12-31 22:54:15 +0900

    add dummy data

---
+++ events.ts
@@ -0,0 +1,37 @@
+export const events = [
+  {
+    id: 1,
+    title: "Random title",
+    createdAt: new Date(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+  },
+  {
+    id: 2,
+    title: "Random title",
+    createdAt: new Date(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+  },
+  {
+    id: 3,
+    title: "Random title",
+    createdAt: new Date(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+  },
+  {
+    id: 4,
+    title: "Random title",
+    createdAt: new Date(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+  },
+  {
+    id: 5,
+    title: "Random title",
+    createdAt: new Date(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+  },
+];
