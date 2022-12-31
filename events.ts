078bfdd2b5101e3e20004f2a17c47e8dd3e8690f
Yusuke <yhakamaya@adobe.com>
2023-01-01 00:11:56 +0900

    update events according to event & payment types

--- events.ts
+++ events.ts
@@ -1,37 +1,69 @@
-export const events = [
+import { Event } from "./types/event";
+
+export const events: Array<Event> = [
   {
-    id: 1,
-    title: "Random title",
-    createdAt: new Date(),
+    id: "1",
+    title: "Event 1",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere nunc vel est placerat consequat. Pellentesque malesuada pharetra risus. ",
+    date: "2020-01-01",
+    createdAt: new Date().toISOString(),
+    imageUrl: "https://picsum.photos/200/300",
+    payments: [
+      {
+        id: "1",
+        title: "Payment 1",
+        amount: 100,
+        createdAt: new Date().toISOString(),
+        paidBy: "Yusuke",
+      },
+      {
+        id: "2",
+        title: "Payment 2",
+        amount: 200,
+        createdAt: new Date().toISOString(),
+        paidBy: "Yusuke",
+      },
+    ],
   },
   {
-    id: 2,
-    title: "Random title",
-    createdAt: new Date(),
+    id: "2",
+    title: "Event 2",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere nunc vel est placerat consequat. Pellentesque malesuada pharetra risus. ",
+    date: "2020-01-02",
+    createdAt: new Date().toISOString(),
+    imageUrl: "https://picsum.photos/200/300",
+    payments: [],
   },
   {
-    id: 3,
-    title: "Random title",
-    createdAt: new Date(),
+    id: "3",
+    title: "Event 3",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere nunc vel est placerat consequat. Pellentesque malesuada pharetra risus. ",
+    date: "2020-01-03",
+    createdAt: new Date().toISOString(),
+    imageUrl: "https://picsum.photos/200/300",
+    payments: [],
   },
   {
-    id: 4,
-    title: "Random title",
-    createdAt: new Date(),
+    id: "4",
+    title: "Event 4",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere nunc vel est placerat consequat. Pellentesque malesuada pharetra risus. ",
+    date: "2020-01-04",
+    createdAt: new Date().toISOString(),
+    imageUrl: "https://picsum.photos/20/300",
+    payments: [],
   },
   {
-    id: 5,
-    title: "Random title",
-    createdAt: new Date(),
+    id: "5",
+    title: "Event 5",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc. Sed euismod, nisl vel ultricies lacinia, nunc nisl ultricies nunc, vel ultricies lorem ipsum sed nunc.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere nunc vel est placerat consequat. Pellentesque malesuada pharetra risus. ",
+    date: "2020-01-05",
+    createdAt: new Date().toISOString(),
+    imageUrl: "https://picsum.photos/200/300",
+    payments: [],
   },
 ];
