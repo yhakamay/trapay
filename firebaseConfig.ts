16f5b56273f39925e15e5b8772c0b3f438b2dd8d
Yusuke <yhakamaya@adobe.com>
2023-01-01 01:17:36 +0900

    add auth

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkjud-CozvFv08wH2XKcVEoFqO0C3DyvI",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
