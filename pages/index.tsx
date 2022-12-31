69139ca0dedf253a7374ad64b055668fb3e57b0b
Yusuke <yhakamaya@adobe.com>
2023-01-01 00:49:49 +0900

    wrap EventCard with WrapItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Box, Wrap } from "@chakra-ui/react";
+import { Box, Wrap, WrapItem } from "@chakra-ui/react";
 import { events } from "../events";
 import EventCard from "../components/molecules/event_card";
 
@@ -18,13 +18,17 @@ export default function Home() {
       <Box px={{ base: "4", md: "8" }}>
         <Wrap>
           {events.map((event) => (
-            <EventCard
-              key={event.id}
-              id={event.id.toString()}
-              title={event.title}
-              createdAt={event.createdAt}
-              description={event.description}
-            />
+            <>
+              <WrapItem>
+                <EventCard
+                  key={event.id}
+                  id={event.id.toString()}
+                  title={event.title}
+                  createdAt={event.createdAt}
+                  description={event.description}
+                />
+              </WrapItem>
+            </>
           ))}
         </Wrap>
       </Box>
