70d8ee5bf4c55c89fbcda695d92ba6f876644a3a
Yusuke <yhakamaya@adobe.com>
2023-01-01 20:13:38 +0900

    change the order
    new payment first, payments second

--- pages/e/[id].tsx
+++ pages/e/[id].tsx
@@ -85,6 +85,57 @@ export default function EventDetails(props: EventDetailsProps) {
               <Spacer />
             </VStack>
           </Card>
+          <Box w={{ base: "sm", md: "lg" }}>
+            <HStack spacing="4">
+              <VStack w="full">
+                <Input
+                  onChange={(e) => setNewPaymentTitle(e.target.value)}
+                  value={newPaymentTitle}
+                  placeholder="Title"
+                />
+                <HStack spacing="4" w="full">
+                  <Input
+                    onChange={(e) =>
+                      setNewPaymentAmount(Number(e.target.value))
+                    }
+                    value={newPaymentAmount}
+                    placeholder="Amount"
+                    type="number"
+                  />
+                  <Menu>
+                    <MenuButton
+                      w="full"
+                      as={Button}
+                      rightIcon={<ChevronDownIcon />}
+                    >
+                      Paid by
+                    </MenuButton>
+                    <MenuList>
+                      {members?.map((member) => (
+                        <MenuItem
+                          key={member.id}
+                          onClick={() => setNewPaymentBy(member)}
+                        >
+                          <Avatar
+                            size="sm"
+                            src={member.photoURL ?? undefined}
+                            name={member.name}
+                            mr="2"
+                          />
+                          {member.name}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </Menu>
+                </HStack>
+              </VStack>
+              <IconButton
+                icon={<AddIcon />}
+                onClick={addPayment}
+                aria-label={"add"}
+              />
+            </HStack>
+          </Box>
           <VStack divider={<StackDivider />} spacing="4">
             {payments?.map((payment) => (
               <Box key={payment.id} w={{ base: "sm", md: "lg" }}>
@@ -108,57 +159,6 @@ export default function EventDetails(props: EventDetailsProps) {
                 </HStack>
               </Box>
             ))}
-            <Box w={{ base: "sm", md: "lg" }}>
-              <HStack spacing="4">
-                <VStack w="full">
-                  <Input
-                    onChange={(e) => setNewPaymentTitle(e.target.value)}
-                    value={newPaymentTitle}
-                    placeholder="Title"
-                  />
-                  <HStack spacing="4" w="full">
-                    <Input
-                      onChange={(e) =>
-                        setNewPaymentAmount(Number(e.target.value))
-                      }
-                      value={newPaymentAmount}
-                      placeholder="Amount"
-                      type="number"
-                    />
-                    <Menu>
-                      <MenuButton
-                        w="full"
-                        as={Button}
-                        rightIcon={<ChevronDownIcon />}
-                      >
-                        Paid by
-                      </MenuButton>
-                      <MenuList>
-                        {members?.map((member) => (
-                          <MenuItem
-                            key={member.id}
-                            onClick={() => setNewPaymentBy(member)}
-                          >
-                            <Avatar
-                              size="sm"
-                              src={member.photoURL ?? undefined}
-                              name={member.name}
-                              mr="2"
-                            />
-                            {member.name}
-                          </MenuItem>
-                        ))}
-                      </MenuList>
-                    </Menu>
-                  </HStack>
-                </VStack>
-                <IconButton
-                  icon={<AddIcon />}
-                  onClick={addPayment}
-                  aria-label={"add"}
-                />
-              </HStack>
-            </Box>
           </VStack>
         </VStack>
       </Box>
