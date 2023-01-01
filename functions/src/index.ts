a1c3d9c509a8c944ed63c769f4f335ca461335f8
Yusuke <yhakamaya@adobe.com>
2023-01-01 18:05:46 +0900

    add createUser function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.createUser = functions.auth.user().onCreate((user) => {
+  return admin.firestore().collection("users").doc(user.uid).set({
+    name: user.displayName,
+    email: user.email,
+    photoURL: user.photoURL,
+  });
+});
