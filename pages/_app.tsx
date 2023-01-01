973797ff179cdbbddc48170a426ec1da06482e03
Yusuke <yhakamaya@adobe.com>
2023-01-01 21:49:39 +0900

    set up nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import Header from "../components/organisms/header";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
