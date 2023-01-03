b4a4c56a6fc1a5f4eed0534cea9a8dd278ab8df4
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-01-03 18:58:53 +0900

    use h prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,6 +71,7 @@ export default function Home() {
                   title={event.title}
                   date={event.date ?? ""}
                   description={event.description ?? ""}
+                  h="md"
                 />
               </WrapItem>
             ))}
