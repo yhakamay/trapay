58edc22b978c4f28ad5131a02ba8170b55ebf813
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-01-04 22:33:12 +0900

    add NewEventForm and use it

---
+++ components/organisms/new_event_form.tsx
@@ -0,0 +1,165 @@
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  InputGroup,
+  Input,
+  Textarea,
+  InputRightElement,
+  IconButton,
+  Wrap,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import {
+  collection,
+  addDoc,
+  CollectionReference,
+  setDoc,
+  doc,
+} from "firebase/firestore";
+import { User as FirebaseUser } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { db } from "../../firebaseConfig";
+import { Event, eventConverter } from "../../types/event";
+import { User, userConverter } from "../../types/user";
+import Loading from "../atoms/loading";
+import UserTag from "../atoms/user_tag";
+
+type NewEventFormProps = {
+  firebaseUser: FirebaseUser;
+};
+
+export default function NewEventForm(props: NewEventFormProps) {
+  const { firebaseUser } = props;
+  const router = useRouter();
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [newMemberName, setNewMemberName] = useState<string>("");
+  const user = convertToUser(firebaseUser);
+  const [members, setMembers] = useState<User[]>([user]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  if (!router.isReady) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <InputGroup>
+        <Input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+      </InputGroup>
+      <Textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description (optional)"
+        resize="none"
+      />
+      <InputGroup>
+        <Input
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          placeholder="Select Date and Time"
+          size="md"
+          type="date"
+        />
+      </InputGroup>
+      <InputGroup>
+        <Input
+          value={newMemberName}
+          onChange={(e) => setNewMemberName(e.target.value)}
+          placeholder="Members"
+        />
+        <InputRightElement mr="2">
+          <IconButton
+            onClick={onClickAddMember}
+            disabled={newMemberName === ""}
+            icon={<AddIcon />}
+            aria-label={"add"}
+            variant="unstyled"
+          />
+        </InputRightElement>
+      </InputGroup>
+      <Wrap>
+        {members.map((member, i) => (
+          <UserTag
+            key={i}
+            user={member}
+            deletable={member.id !== user.id}
+            onDelete={() => setMembers(members.filter((m) => m !== member))}
+          />
+        ))}
+      </Wrap>
+      <Box h="8" />
+      <Button isLoading={loading} onClick={onClickSaveEvent}>
+        Save
+      </Button>
+    </>
+  );
+
+  function convertToUser(firebaseUser: FirebaseUser) {
+    return {
+      id: firebaseUser.uid!,
+      name: firebaseUser.displayName ?? "",
+      email: firebaseUser.email ?? "",
+      photoURL: firebaseUser.photoURL ?? "",
+    };
+  }
+
+  function onClickAddMember() {
+    const newMember: User = {
+      id: null,
+      name: newMemberName,
+      email: null,
+      photoURL: null,
+    };
+    setMembers([...members, newMember]);
+    setNewMemberName("");
+  }
+
+  function onClickSaveEvent() {
+    setLoading(true);
+    saveEvent();
+    setLoading(false);
+  }
+
+  async function saveEvent(): Promise<void> {
+    const event: Event = {
+      title,
+      description,
+      date,
+      imageUrl: null,
+    };
+
+    // Save this event to the Firestore events/id then redirect to the event page
+    const eventsRef = collection(db, "events").withConverter(eventConverter);
+    const eventRef = await addDoc(eventsRef, event);
+
+    // Add members to the members collection
+    const membersRef = collection(eventRef, "members").withConverter(
+      userConverter
+    );
+
+    await Promise.all(
+      members.map((memberName) => addMember(membersRef, memberName))
+    );
+
+    // Redirect to the event page
+    router.push(`/e/${eventRef.id}`);
+  }
+
+  async function addMember(
+    membersRef: CollectionReference<User>,
+    member: User
+  ): Promise<void> {
+    if (member.id) {
+      setDoc<User>(doc(membersRef, member.id), member);
+    } else {
+      await addDoc<User>(membersRef, member);
+    }
+  }
+}
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,52 +1,17 @@
-import { AddIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Center,
-  Heading,
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Textarea,
-  VStack,
-  Wrap,
-} from "@chakra-ui/react";
-import {
-  addDoc,
-  collection,
-  CollectionReference,
-  doc,
-  setDoc,
-} from "firebase/firestore";
+import { Box, Center, Heading, VStack } from "@chakra-ui/react";
+
 import Head from "next/head";
 import NextImage from "next/image";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import router from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Loading from "../components/atoms/loading";
-import UserTag from "../components/atoms/user_tag";
-import { auth, db } from "../firebaseConfig";
-import { Event, eventConverter } from "../types/event";
-import { User, userConverter } from "../types/user";
+import NewEventForm from "../components/organisms/new_event_form";
+import { auth } from "../firebaseConfig";
 
 export default function NewEvent() {
   const [user, loadingUser] = useAuthState(auth);
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [date, setDate] = useState<string>("");
-  const [newMemberName, setNewMemberName] = useState<string>("");
-  const me: User = {
-    id: auth.currentUser?.uid ?? "",
-    name: auth.currentUser?.displayName ?? "",
-    email: auth.currentUser?.email ?? "",
-    photoURL: auth.currentUser?.photoURL ?? "",
-  };
-  const [members, setMembers] = useState<User[]>([me]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
 
-  if (!router.isReady || loadingUser) {
+  if (loadingUser) {
     return <Loading />;
   }
 
@@ -69,112 +34,10 @@ export default function NewEvent() {
               width="200"
               height="200"
             />
-            <InputGroup>
-              <Input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Title"
-              />
-            </InputGroup>
-            <Textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Description (optional)"
-              resize="none"
-            />
-            <InputGroup>
-              <Input
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="Select Date and Time"
-                size="md"
-                type="date"
-              />
-            </InputGroup>
-            <InputGroup>
-              <Input
-                value={newMemberName}
-                onChange={(e) => setNewMemberName(e.target.value)}
-                placeholder="Members"
-              />
-              <InputRightElement mr="2">
-                <IconButton
-                  onClick={() => {
-                    const newMember: User = {
-                      id: null,
-                      name: newMemberName,
-                      email: null,
-                      photoURL: null,
-                    };
-                    setMembers([...members, newMember]);
-                    setNewMemberName("");
-                  }}
-                  disabled={newMemberName === ""}
-                  icon={<AddIcon />}
-                  aria-label={"add"}
-                  variant="unstyled"
-                />
-              </InputRightElement>
-            </InputGroup>
-            <Wrap>
-              {members.map((member, i) => (
-                <UserTag
-                  key={i}
-                  user={member}
-                  deletable={member.id !== me.id}
-                  onDelete={() =>
-                    setMembers(members.filter((m) => m !== member))
-                  }
-                />
-              ))}
-            </Wrap>
-            <Box h="8" />
-            <Button isLoading={loading} onClick={saveEvent}>
-              Save
-            </Button>
+            <NewEventForm firebaseUser={user!} />
           </VStack>
         </Box>
       </Center>
     </>
   );
-
-  async function saveEvent(): Promise<void> {
-    setLoading(true);
-
-    const event: Event = {
-      title,
-      description,
-      date,
-      imageUrl: null,
-    };
-
-    // Save this event to the Firestore events/id then redirect to the event page
-    const eventsRef = collection(db, "events").withConverter(eventConverter);
-    const eventRef = await addDoc(eventsRef, event);
-
-    // Add members to the members collection
-    const membersRef = collection(eventRef, "members").withConverter(
-      userConverter
-    );
-
-    await Promise.all(
-      members.map((memberName) => addMember(membersRef, memberName))
-    );
-
-    setLoading(false);
-
-    // Redirect to the event page
-    router.push(`/e/${eventRef.id}`);
-  }
-
-  async function addMember(
-    membersRef: CollectionReference<User>,
-    member: User
-  ): Promise<void> {
-    if (member.id) {
-      setDoc<User>(doc(membersRef, member.id), member);
-    } else {
-      await addDoc<User>(membersRef, member);
-    }
-  }
 }
