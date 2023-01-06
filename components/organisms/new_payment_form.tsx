2d64a8c7afb6e148a6515ecb09759ef43516cda0
Yusuke <yhakamaya@adobe.com>
2023-01-06 19:56:47 +0900

    wrap the NewPaymentForm with Card

--- components/organisms/new_payment_form.tsx
+++ components/organisms/new_payment_form.tsx
@@ -10,6 +10,10 @@ import {
   Avatar,
   IconButton,
   Box,
+  Card,
+  CardBody,
+  Heading,
+  Stack,
 } from "@chakra-ui/react";
 import { addDoc, collection, DocumentReference } from "firebase/firestore";
 import { useState } from "react";
@@ -42,55 +46,66 @@ export default function NewPaymentForm(props: NewPaymentFormProps) {
 
   return (
     <Box w={{ base: "sm", md: "lg" }}>
-      <HStack spacing="4">
-        <VStack w="full">
-          <Input
-            onChange={(e) => setNewPaymentTitle(e.target.value)}
-            value={newPaymentTitle}
-            placeholder="Title"
-          />
-          <HStack spacing="4" w="full">
-            <Input
-              onChange={(e) => setNewPaymentAmount(Number(e.target.value))}
-              value={newPaymentAmount || undefined}
-              placeholder="Amount"
-              type="number"
-            />
-            <Menu>
-              <MenuButton
-                w="full"
-                as={Button}
-                rightIcon={<MdExpandMore />}
-                variant="outline"
-              >
-                Paid by
-              </MenuButton>
-              <MenuList>
-                {members?.map((member) => (
-                  <MenuItem
-                    key={member.id}
-                    onClick={() => setNewPaymentBy(member)}
-                  >
-                    <Avatar
-                      size="sm"
-                      src={member.photoURL ?? undefined}
-                      name={member.name}
-                      mr="2"
-                    />
-                    {member.name}
-                  </MenuItem>
-                ))}
-              </MenuList>
-            </Menu>
-          </HStack>
-        </VStack>
-        <IconButton
-          disabled={!newPaymentTitle || !newPaymentAmount || !newPaymentBy}
-          icon={<MdAdd />}
-          onClick={addPayment}
-          aria-label={"add"}
-        />
-      </HStack>
+      <Card variant="outline">
+        <CardBody>
+          <Stack spacing="4">
+            <Heading size="sm">New payment</Heading>
+            <HStack spacing="4">
+              <VStack w="full">
+                <Input
+                  onChange={(e) => setNewPaymentTitle(e.target.value)}
+                  value={newPaymentTitle}
+                  placeholder="Title"
+                />
+                <HStack spacing="4" w="full">
+                  <Input
+                    onChange={(e) =>
+                      setNewPaymentAmount(Number(e.target.value))
+                    }
+                    value={newPaymentAmount || undefined}
+                    placeholder="Amount"
+                    type="number"
+                  />
+                  <Menu>
+                    <MenuButton
+                      w="full"
+                      as={Button}
+                      rightIcon={<MdExpandMore />}
+                      variant="outline"
+                    >
+                      Paid by
+                    </MenuButton>
+                    <MenuList>
+                      {members?.map((member) => (
+                        <MenuItem
+                          key={member.id}
+                          onClick={() => setNewPaymentBy(member)}
+                        >
+                          <Avatar
+                            size="sm"
+                            src={member.photoURL ?? undefined}
+                            name={member.name}
+                            mr="2"
+                          />
+                          {member.name}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </Menu>
+                </HStack>
+              </VStack>
+              <IconButton
+                disabled={
+                  !newPaymentTitle || !newPaymentAmount || !newPaymentBy
+                }
+                icon={<MdAdd />}
+                onClick={addPayment}
+                aria-label={"add"}
+              />
+            </HStack>
+          </Stack>
+        </CardBody>
+      </Card>
     </Box>
   );
 
