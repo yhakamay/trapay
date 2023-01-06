bedc0795eeb38a57cdb92860388bc389192c0612
Yusuke <yhakamaya@adobe.com>
2023-01-06 22:18:59 +0900

    show paid amount per person

--- components/molecules/summary_card.tsx
+++ components/molecules/summary_card.tsx
@@ -1,8 +1,18 @@
-import { Card, VStack, Spacer, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  Heading,
+  Stack,
+  CardBody,
+  Box,
+  Text,
+  StackDivider,
+} from "@chakra-ui/react";
 import { collection, DocumentReference } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Event } from "../../types/event";
-import { paymentConverter } from "../../types/payment";
+import { Payment, paymentConverter } from "../../types/payment";
+import { User, userConverter } from "../../types/user";
+import Loading from "../atoms/loading";
 
 type SummaryCardProps = {
   eventRef: DocumentReference<Event>;
@@ -13,20 +23,66 @@ export default function SummaryCard(props: SummaryCardProps) {
   const paymentsRef = collection(eventRef, "payments").withConverter(
     paymentConverter
   );
-  const [payments] = useCollectionData(paymentsRef);
+  const [payments, loadingPayments] = useCollectionData(paymentsRef);
+  const membersRef = collection(eventRef, "members").withConverter(
+    userConverter
+  );
+  const [members, loadingMembers] = useCollectionData(membersRef);
+  const total = getTotal(payments ?? []);
+  const perPerson = getPerPerson(total, members ?? []);
+  const membersWithPaymentAmount = getMembersWithPaymentAmount(members ?? []);
+
+  if (loadingPayments || loadingMembers) return <Loading />;
 
   return (
     <Card w={{ base: "sm", md: "lg" }} variant="outline">
-      <VStack>
-        <Spacer />
-        <Text>Total</Text>
-        <Heading size="lg">{getTotal()}</Heading>
-        <Spacer />
-      </VStack>
+      <CardBody>
+        <Stack>
+          <Heading size="sm">Total</Heading>
+          <Text>{total}</Text>
+          <Heading size="sm">Per person</Heading>
+          <Text>{`${perPerson} / person`}</Text>
+          <Box h="4" />
+          <Stack divider={<StackDivider />} spacing="4">
+            {membersWithPaymentAmount.map((member) => (
+              <Box key={member.id}>
+                <Heading size="xs">{member.name}</Heading>
+                <Text pt="2" fontSize="sm">
+                  {member.payment}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        </Stack>
+      </CardBody>
     </Card>
   );
 
-  function getTotal() {
-    return payments?.reduce((acc, payment) => acc + payment.amount, 0);
+  function getTotal(payments: Payment[]): number {
+    return payments?.reduce((acc, payment) => acc + payment.amount, 0) ?? 0;
+  }
+
+  function getPerPerson(total: number, members: User[]): number {
+    return Math.ceil(total / (members?.length ?? 1));
+  }
+
+  function getMembersWithPaymentAmount(
+    members: User[]
+  ): UserWithPaymentAmount[] {
+    return (
+      members?.map((member) => {
+        const paymentAmount = payments
+          ?.filter((payment) => payment.paidBy.id === member.id)
+          .reduce((acc, payment) => acc + payment.amount, 0);
+        return {
+          ...member,
+          payment: paymentAmount,
+        };
+      }) ?? []
+    );
   }
 }
+
+type UserWithPaymentAmount = User & {
+  payment: number | undefined;
+};
