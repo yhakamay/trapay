0d7fb963b0b0b305dfdc9ad9dfd150608533d522
Yusuke <yhakamaya@adobe.com>
2023-01-08 20:56:58 +0900

    add TransactionsList

---
+++ components/molecules/transactions_list.tsx
@@ -0,0 +1,35 @@
+import { User as FirebaseUser } from "firebase/auth";
+import { Stack, StackDivider, Heading, Box, Text } from "@chakra-ui/react";
+import { Transaction } from "../../types/transaction";
+
+type TransactionsListProps = {
+  user: FirebaseUser;
+  transactions: Transaction[];
+};
+
+export default function TransactionsList(props: TransactionsListProps) {
+  const { user, transactions } = props;
+
+  return (
+    <Stack divider={<StackDivider />} spacing="4">
+      {transactions.map((transaction) => {
+        const isPayee = transaction.to.id === user.uid;
+        const isPayer = transaction.from.id === user.uid;
+
+        return (
+          <Box key={transaction.id}>
+            <Heading size="xs">{`${transaction.from.name} → ${transaction.to.name}`}</Heading>
+            <Text
+              pt="2"
+              fontSize="sm"
+              color={isPayee ? "green.500" : isPayer ? "red.500" : undefined}
+              fontWeight={isPayee || isPayer ? "bold" : undefined}
+            >
+              {transaction.amount}
+            </Text>
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+}
