2c89c5a3d5b6714729e171093c684995e081e98b
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-01-10 12:16:17 +0900

    add dark mode

--- theme.ts
+++ theme.ts
@@ -1,11 +1,18 @@
 import {
-  baseTheme,
+  theme as baseTheme,
   extendTheme,
   withDefaultColorScheme,
+  ThemeConfig,
 } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
 
 export const theme = extendTheme(
   {
+    config,
     colors: {
       brand: baseTheme.colors.purple,
     },
