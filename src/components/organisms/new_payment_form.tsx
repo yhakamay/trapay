551f193d37f3ca6eee98b313c577003765d36c6d
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-01-22 14:09:18 +0900

    i18n for add member

--- src/components/organisms/new_payment_form.tsx
+++ src/components/organisms/new_payment_form.tsx
@@ -108,7 +108,7 @@ export default function NewPaymentForm(props: NewPaymentFormProps) {
                             onOpen();
                           }}
                         >
-                          Add member
+                          {t.addMember}
                         </MenuItem>
                       </MenuList>
                     </Menu>
